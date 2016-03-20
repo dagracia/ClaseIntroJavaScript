5e355f83ee378d61c0a3668237df28d45c565f03
shirleyandrea2016 <smirabap@gmail.com>
2016-03-20 15:53:15 -0500

    correcciones de Shirley

--- scripts.js
+++ scripts.js
@@ -1,11 +1,5 @@
-console.log("pon el punto y coma")
-alert("le falta el parentesis";
-document.write("<h1>cierra el tag");
-aler("es alert");
-consol.log("se escribe console");
-
-console.("pon el log ");
-lert("alert comienza con a");
-Document.write("<h1>Documente es con d minuscula</h1>");
-Document.("<h1>Falta Write</h1>");
-console.log(va entre comillas);
+console.log("pon el punto y coa");
+alert("le falta el parentesis");
+document.write("<h1>jholaaaaaaaaaaa</h1>");
+alert("inicializando");
+console.log("hiiiiii");
